74886ca5d0b82d01d4867496781236bb27abcfec
KONGYKHANG <124757841+KONGYKHANG@users.noreply.github.com>
2025-01-27 17:27:26 +0700

    new Update Project

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function myFunction() {
+    var name = "បានកក់ជោគជ័យ"
+    document.getElementById("button-1").innerHTML = name;
+    if(name == "បានកក់ជោគជ័យ"){
+        alert("កក់ទុកក្នុងប្រតិទិន")
+    }
+    else if(name != "មិនបានកក់ជោគជ័យ"){
+        alert("មិនបានកក់ជោគជ័យ")
+    }
+  }
